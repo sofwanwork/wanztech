ecfadb1d62498116cd043c43ccb3372bc30ba4e5
WanzTech <wanztech@example.com>
2026-02-08 01:01:10 +0800

    fix: reduce bold weight to 700 but keep high contrast

--- app/(public)/form/[id]/client.tsx
+++ app/(public)/form/[id]/client.tsx
@@ -396,7 +396,7 @@ export function PublicFormClient({ form }: PublicFormClientProps) {
               </CardTitle>
               {form.description && (
                 <div
-                  className="text-gray-600 mt-2 [&>p]:mb-0 [&>p:empty]:h-3 [&_strong]:font-black [&_strong]:text-black [&_b]:font-black [&_b]:text-black font-sans"
+                  className="text-gray-600 mt-2 [&>p]:mb-0 [&>p:empty]:h-3 [&_strong]:font-bold [&_strong]:text-black [&_b]:font-bold [&_b]:text-black font-sans"
                   dangerouslySetInnerHTML={{ __html: form.description }}
                 />
               )}
